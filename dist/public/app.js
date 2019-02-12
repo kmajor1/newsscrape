87ede43e8fd400f8b8793b8007e3cf38816fa2d2
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-02-12 13:11:58 -0500

    built

--- dist/public/app.js
+++ dist/public/app.js
@@ -14,7 +14,6 @@ function getArticles() {
       $("#scrapeNow").text('News Pulled!');
       $("#scrapeNow").addClass('disabled');
       $('[data-toggle="tooltip"]').tooltip({
-
         delay: 250
       });
 
