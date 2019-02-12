a84dc57d9ebb21cf97f7cb93e93b0919c456dbb1
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2019-02-12 02:48:00 -0500

    t

--- dist/public/app.js
+++ dist/public/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// get articles function (wrapping this)
+// Function to grab all articles stored in database
 function getArticles() {
   console.log('Call get articles');
   $.getJSON('/articles', function (data) {
@@ -17,25 +17,30 @@ function getArticles() {
   });
 }
 
+// add comment behaviour 
+// display form for user to enter a comment when user clicks headlines
+
 $(document).on('click', 'p', function () {
   $("#comments").empty();
   var clickedId = $(this).attr('data-id');
 
-  // go get the article
+  // GET request to retrieve article that was clicked 
+
   $.ajax({
     method: 'GET',
     url: '/articles/' + clickedId,
     dataType: 'json'
   }).then(function (articleData) {
     // article title 
-    console.log('Running promise of get specific article');
     console.log(articleData);
     $("#comments").append("<h4>" + articleData.headline + "</h4>");
     $("#comments").append('<div class="form-group"><input class="form-control" id="inputTitle" type="text" name="title" placeholder="Give it a title"></div><div class="form-group"><textarea class="form-control" id="inputBody" name="body">Type your comment</textarea></div><button id="submitComment" type="submit" class="btn btn-primary">Comment Now</button>');
 
-    // add current note, if there is one 
+    // add current comment, if there is one 
     if (articleData.comment) {
       console.log(articleData.comment);
+      $("#inputTitle").val(articleData.comment.title);
+      $("#inputBody").val(articleData.comment.body);
     }
   });
 });
